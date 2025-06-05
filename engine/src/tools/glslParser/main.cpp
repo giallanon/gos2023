98a9e139cd1ba314589ba19e52dce7038ee75d80
Gianluca Brunelli <gbrunelli@rheavendors.com>
2025-06-05 14:03:38 +0200

    aggiunto un esempio di come compilare gli shader via glslc e popen

--- engine/src/tools/glslParser/main.cpp
+++ engine/src/tools/glslParser/main.cpp
@@ -3,7 +3,32 @@
 using namespace gos;
 
 
+//******************************** 
+bool compile (const char *shaderSRCFile, const char *shaderStage, bool bWithSourceLevelDebugInfo)
+{
+    //glslc -fshader-stage=vert --target-env=vulkan1.3 lineRenderer.vert.shader -g -O -o lineRenderer.vert.spv
+    char cmd[1024];
+    sprintf_s (cmd, sizeof(cmd), "glslc -fshader-stage=%s --target-env=vulkan1.3 %s/example/%s -g -O -o %s/example/compiled/%s.spv", 
+        shaderStage,
+        gos::getAppPathNoSlash(), shaderSRCFile,
+        gos::getAppPathNoSlash(), shaderSRCFile
+    );
 
+    FILE *fp = _popen (cmd, "r");
+    if (NULL != fp)
+    {
+        memset (cmd, 0, sizeof(cmd));
+        while (fgets(cmd, sizeof(cmd), fp) != NULL)
+            printf("%s", cmd);
+        _pclose(fp);
+    }
+
+    _getch();
+
+    if (0x00 == cmd[0])
+        return true;
+    return false;
+}
 
 //******************************** 
 int main()
@@ -16,6 +41,7 @@ int main()
         return -1;
     else
     {
+        compile("lineRenderer.vert.shader", "vert", true); return 0;
         fs::addAlias ("@ex", "example", eAliasPathMode::relativeToAppFolder);
 
         SPVReflect parser;
